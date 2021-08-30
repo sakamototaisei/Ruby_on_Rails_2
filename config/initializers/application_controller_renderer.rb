042c2438d3213ec05b0938ce64f0cd120a03837e
sakamototaisei <taisei0403089@gmail.com>
2021-08-30 14:17:14 +0000

    Rails4章復習

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '2e291545766ba9186c690732b4d1cf2d067bd9c3217b79b879c378300ae7ee7d3c75118c8c42cd73a1210893de686f537e9e4ef7db7ca40b4c38181d8486d60a'
